9ffa9e98fafa8aa733e5b7225b91e3307437a657
saiteja10 <ss6q9@mail.umkc.edu>
2015-02-21 20:47:25 -0600

    1st

---
+++ src/com/example/FrontEnd_PG4/MyActivity1.java
@@ -0,0 +1,15 @@
+package com.example.FrontEnd_PG4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sai on 2/21/2015.
+ */
+public class MyActivity1 extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main1);
+    }
+}
